dd6b49b88cdf27572b101a99f2bd9d34569da435
Thai <thaianesousa28@gmail.com>
2024-12-03 11:32:43 -0300

    Aula 2 sobre Variável

---
+++ AULA  2 THAIANE/AULA 02.py
@@ -0,0 +1,11 @@
+print('O que é que Cacá quer? Cacá quer caqui. Qual caqui que Cacá quer? Cacá quer qualquer caqui')
+
+nome_usuario = 'Thaiane' # tipo -> str = string
+idade_usuario = 19 # tipo -> int = integer
+altura_usuario = 1.60 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print('Seja Bem-Vindo',nome_usuario)
+print('Sua idade é', idade_usuario, 'anos')
+print('você tem', altura_usuario, "metros.")
+print('Você é maior de idade?' , maior_idade)
